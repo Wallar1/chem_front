e7f05cccd785c0a22a93d8eb7424b1e808ae6261
Craig Wallace <Robertwallace.naples@gmail.com>
2022-12-21 14:26:20 -0800

    Updated scientist cards

--- web_app/src/components/scientist_timeline/scientists.js
+++ web_app/src/components/scientist_timeline/scientists.js
@@ -18,40 +18,64 @@
 */
 
 export const scientists = {
-    'MaxPlanck': {
-        name: 'Max Planck',
-        src: 'https://t3.gstatic.com/licensed-image?q=tbn:ANd9GcStjVkByJ0OkjZHEdoqZtfws_WnkdbJh-piwo3ia607u-CxoB6v--8yrOsZzdhRivcTamp3qzWuPsgp0KY'
+    'Democritus': {
+        'name': 'Democritus',
+        'year': '380 BC',
+        'story': 'Declared that matter is composed of indivisible and indestructible particles called "atomos"',
+        'src': 'https://www.lookingforwisdom.com/content/images/size/w2000/2020/11/Coypel_Democritus.jpg',
     },
-    'ErnestRutherford': {
-        name: 'Ernest Rutherford',
-        src: 'https://physicsworld.com/wp-content/uploads/2019/06/1909Riceamr.jpg'
+    'Geber': {
+        'name': 'Jābir ibn Hayyān (Geber)',
+        'story': 'Father of chemistry: introduced a systematic classification of chemical substances, and provided instructions for deriving an inorganic compound (sal ammoniac or ammonium chloride) from organic substances (such as plants, blood, and hair) by chemical means. Declared there were 6 idealized elements: air, earth, fire, water, sulphur (combustability), and mercury (metallic properties).',
+        'year': '800',
+        'src': 'https://www.bbvaopenmind.com/wp-content/uploads/2021/04/BBVA-OpenMind-Miguel-Barral-Jabir-ibn-Hayyan-2.jpg'
+    },
+    'SirFrancisBacon': {
+        'name': 'Sir Francis Bacon',
+        'story': 'Developed the scientific method',
+        'year': '1605',
+        'src': 'https://www.biography.com/.image/t_share/MTIwNjA4NjMzNzMxNzc4MDYw/francis-bacon-9194632-1-402.jpg'
     },
     'RobertBoyle': {
-        name: 'Robert Boyle',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/b/b3/Robert_Boyle_0001.jpg'
+        'name': 'Robert Boyle',
+        'story': "Founder of modern chemistry. Boyle's law states that the volume and pressure of a gas are inversely proportional when the temperature is constant. So when the pressure goes up, the volume goes down, and vice versa",
+        'year': '1662',
+        'src': 'https://i0.wp.com/theirishspirit.com/wp-content/uploads/2022/01/Robert-Boyle.jpg?fit=1326%2C986&ssl=1'
     },
-    'MaxPlanck2': {
-        name: 'Max Planck',
-        src: 'https://t3.gstatic.com/licensed-image?q=tbn:ANd9GcStjVkByJ0OkjZHEdoqZtfws_WnkdbJh-piwo3ia607u-CxoB6v--8yrOsZzdhRivcTamp3qzWuPsgp0KY'
+    AlessandroVolta: {
+        name: 'Alessandro Volta',
+        story: 'Constructed the first electrical battery and founded electrochemistry with the invention of galvonic cells.',
+        year: '1800',
+        src: 'https://upload.wikimedia.org/wikipedia/commons/5/52/Alessandro_Volta.jpeg'
     },
-    'ErnestRutherford2': {
-        name: 'Ernest Rutherford',
-        src: 'https://physicsworld.com/wp-content/uploads/2019/06/1909Riceamr.jpg'
+    Lavoisier: {
+        name: 'Antoine-Laurent de Lavoisier',
+        story: "Brought about a rennaissance in chemistry by discovering the Law of Conservation of Mass, inventing our system of naming with Berthollet, working with Laplace on calorimetry, and understanding combustion with his wife.",
+        year: '1780',
+        src: 'https://www.sciencehistory.org/sites/default/files/styles/rte_full_width/public/lavoisier3.jpg?itok=7fZV0U1C'
     },
-    'RobertBoyle2': {
-        name: 'Robert Boyle',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/b/b3/Robert_Boyle_0001.jpg'
+    DuPont: {
+        name: 'Éleuthère Irénée du Pont',
+        story: 'Invented gun powder and founded the DuPont chemical company. Studied under Lavoisier.',
+        year: '1802',
+        src: 'https://worldbookblog.files.wordpress.com/2015/06/pc369888-src.jpg'
     },
-    'MaxPlanck3': {
-        name: 'Max Planck',
-        src: 'https://t3.gstatic.com/licensed-image?q=tbn:ANd9GcStjVkByJ0OkjZHEdoqZtfws_WnkdbJh-piwo3ia607u-CxoB6v--8yrOsZzdhRivcTamp3qzWuPsgp0KY'
+    JohnDalton: {
+        name: 'John Dalton',
+        story: "Discovered the relationship between a mixture of gases and their partial pressures (Dalton's Law). Provided a basis for modern atomic theory and stoichiometry.",
+        year: '1803',
+        src: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhdKbVsLm24B8B1n8DZOgsk5Mc-DxqCo7W0f2I6lzqHxummLeW7WH7Lqec2q5ptcVuS4MaRnoOV6-8ZcYYc8VbVVDevAS4fwaD19PEQdVHdmqPiivDxx3uuZA-mZ-jpnakWIAUvXr3-iX4LgLIjyJN3GVOB_zt8Ds51zklPbMx7NXJ--bOqI5Gs2Y8zzA/s1230/John_Dalton_by_Charles_Turner.jpg'
     },
-    'ErnestRutherford3': {
-        name: 'Ernest Rutherford',
-        src: 'https://physicsworld.com/wp-content/uploads/2019/06/1909Riceamr.jpg'
+    Berzelius: {
+        name: 'Jöns Jacob Berzelius',
+        story: "Invented our chemical symbals for elements and subscripts/proportions (eg H2O), as well as identified several new elements (silicon, selenium, thorium, cerium, lithium, and vanadium).",
+        year: '1828',
+        src: 'https://www.worldofchemicals.com/Article/477/Jons%20Jacob%20Berzelius.jpg'
     },
-    'RobertBoyle3': {
-        name: 'Robert Boyle',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/b/b3/Robert_Boyle_0001.jpg'
-    },
+    HumphryDavy: {
+        name: 'Humphry Davy',
+        story: "Discovered several earth metals (sodium, potassium, calcium, magnesium, strontium, and barium), as well as contributions to the discoveries of the elemental nature of chlorine and iodine.",
+        year: '1807',
+        src: 'https://cdn.britannica.com/96/12396-050-A1110D81/Humphry-Davy-Thomas-Lawrence-detail-oil-painting.jpg'
+    }
 }
