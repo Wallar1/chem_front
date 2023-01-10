decabf2e8e3327ab70135ddb05164c15bd8147ec
Craig Wallace <Robertwallace.naples@gmail.com>
2023-01-10 14:05:54 -0800

    Created initial view for compounds to be visualized

--- web_app/src/App.svelte
+++ web_app/src/App.svelte
@@ -3,10 +3,13 @@
 	import {possible_scenes, current_scene} from './stores.js';
     import Timeline from './components/scientist_timeline/scientist_timeline.svelte';
 	import Battle from './components/battle_scene/battle.svelte';
+	import CompoundCreator from './components/compound_creator/main.svelte';
 </script>
 
 <main>
-	{#if $current_scene === possible_scenes.Timeline}
+	{#if true}
+		<CompoundCreator/>
+	{:else if $current_scene === possible_scenes.Timeline}
 		<Timeline/>
 	{:else if $current_scene === possible_scenes.Battle}
 		<Battle/>
---
+++ web_app/src/components/compound_creator/main.svelte
@@ -0,0 +1,19 @@
+
+<script>
+    import { onMount } from 'svelte';
+    import { CompoundCreator } from './compound_creator.js';
+    onMount(async () => {
+        new CompoundCreator();
+    })
+</script>
+
+<div id='outer'>
+    <div id='canvas-container'></div>
+</div>
+
+<style>
+    #outer {
+        background-color: #050505;
+        background: radial-gradient(ellipse at center,  rgba(43,45,48,1) 0%,rgba(0,0,0,1) 100%);
+    }
+</style>
